f9631bdbd3315ba6982f34f7f7ad9bf999ed20b2
Mrityunjay1 <singhmrityunjay32@gmail.com>
2025-07-30 12:16:20 +0530

    Feat: added db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-const Home = () => {
+import prisma from "@/lib/db";
+
+const Home = async () => {
+  const users = await prisma.post.findMany()
   return (
     <div className="font-bold text-rose-500">
-      <h1>Home</h1>
+      <h1>{JSON.stringify(users)}</h1>
     </div>
   );
 };
